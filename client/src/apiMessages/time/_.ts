350145c0dc595f13db97080d233b699febb1c3c5
Kulihrasek456 <70462840+Kulihrasek456@users.noreply.github.com>
2026-08-10 12:46:02 +0200

    apiMessages - added device time cache to Time namespace

--- client/src/apiMessages/time/_.ts
+++ client/src/apiMessages/time/_.ts
@@ -1,6 +1,10 @@
+import { createSignal } from "solid-js";
 import { checkNumber, checkTimestamp, sendJsonApiMessage, type apiMessageOptions } from "../apiMessageBase";
 
 export namespace Time{
+    export let [deviceTime, setDeviceTime] = createSignal<Date | undefined>(undefined)
+    export let [deviceTimeOffset, setDeviceTimeOffset] = createSignal<number | undefined>(undefined)
+    
     export async function getTime() : Promise<Date>{
         let opts : apiMessageOptions = {
             url: "/time",
@@ -14,11 +18,16 @@ export namespace Time{
 
         checkTimestamp(data,"serverTime",opts);
 
-        let responseTime : Date = new Date(data.serverTime);
+        let responseTime: Date = new Date(data.serverTime);
+        let commEndTime = Date.now()
+        let roundTripTime = commEndTime - sendTime;
 
-        let roundTripTime = Date.now() - sendTime;
+        let time = new Date(responseTime.getTime() - (roundTripTime/2))
 
-        return new Date(responseTime.getTime() - (roundTripTime/2))
+        setDeviceTime(time)
+        setDeviceTimeOffset((responseTime.getTime() - commEndTime) - roundTripTime)
+        
+        return time
     }
 
     export type convertTime = {
